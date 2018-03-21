d7a9a2cc0aad5fc510fdf9069a0deeb4d6b72dda
hsandstromOM <hosea@obviouslee.com>
2018-03-21 13:35:51 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.use(express.static(__dirname + '/'));
+
+app.listen(process.env.PORT || 5000);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
+});
